aefe88ea398ee78af5be848d16832c2bc096ab7a
Jack Desert <jackdesert@gmail.com>
2017-02-19 08:26:28 -0600

    placeholder

--- i3_monitor.sh
+++ i3_monitor.sh
@@ -21,24 +21,28 @@ else
   monitor_mode=`cat /tmp/monitor_mode.dat`
 fi
 
-if [ $monitor_mode = "all" ]; then
+if [ $monitor_mode = "internal" ]; then
         monitor_mode="external"
         COMMAND="xrandr --output $INTERNAL_OUTPUT --off --output $EXTERNAL_OUTPUT --auto $EXTERNAL_ROTATION"
-elif [ $monitor_mode = "external" ]; then
+else
         monitor_mode="internal"
         COMMAND="xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --off"
-elif [ $monitor_mode = "internal" ]; then
-        monitor_mode="clones"
-        COMMAND="xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --auto $EXTERNAL_ROTATION --same-as $INTERNAL_OUTPUT"
-else
-        monitor_mode="all"
-        COMMAND="xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --auto $EXTERNAL_ROTATION --above $INTERNAL_OUTPUT"
 fi
+
 echo "${monitor_mode}" > $FILE
 echo writing "$monitor_mode" to $FILE
 echo running command: $COMMAND
+echo ''
+echo $monitor_mode
+echo ''
 $COMMAND
 
 
 # Test
 # xrandr --output 'LVDS' --off --output 'DisplayPort-2' --auto --rotate 'left'
+
+# Other modes
+#   Clone:
+#     COMMAND="xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --auto $EXTERNAL_ROTATION --same-as $INTERNAL_OUTPUT"
+#   All:
+#     COMMAND="xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --auto $EXTERNAL_ROTATION --above $INTERNAL_OUTPUT"
